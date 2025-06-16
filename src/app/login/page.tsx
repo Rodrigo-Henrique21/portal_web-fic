d85b0b3b222edce846624b190b27d7a766ee46f5
Lucas Storch <lucasstorch.ls@gmail.com>
2025-06-16 07:40:20 -0300

    firebase and login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Image from "next/image";
+import { FormEvent, useState } from "react";
+import { login } from "@/hooks/useLogin";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await login(username, password);
+      router.push("/portal");
+    } catch (error) {
+      alert("Erro ao logar: " + (error as Error).message);
+    }
+  };
+
   return (
-    <div className="min-h-screen flex ">
+    <div className="min-h-screen flex">
       <aside className="senai-login-aside h-dvh w-[300px] hidden sm:flex">
         <Image
           width={300}
@@ -25,14 +44,16 @@ export default function Home() {
       <main className="senai-login-main">
         <section className="senai-login-container">
           <h2>Acesse sua conta</h2>
-          <form action="/login" method="POST">
+          <form onSubmit={handleSubmit}>
             <div className="input-group">
               <Label>Usuário</Label>
               <Input
-                type="text"
+                type="email"
                 className="input form-control"
                 id="username"
                 name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                 required
               />
             </div>
@@ -44,6 +65,8 @@ export default function Home() {
                 type="password"
                 id="password"
                 name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required
               />
             </div>
